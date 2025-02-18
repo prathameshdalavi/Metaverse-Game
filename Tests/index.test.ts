f2f8c8bfdc4529a8152a873715b48e3ac49584a3
Prathamesh Dalavi <prathameshdalavi04@gmail.com>
2025-02-18 18:12:10 +0530

    updated

--- Tests/index.test.ts
+++ Tests/index.test.ts
@@ -98,9 +98,32 @@ describe("User metadata endpoints", () => {
     })
     
 })
+describe("User avatar Information", () => {
+    let avatarId;
+    let token ;
+    let userId;
+    beforeAll(async () => {
+        const userName = `prathamesh-${Math.random()}`;
+        const password = "123456";
+        const signupResponse = await axios.post(BACKEND_URL + "/api/v1/signup", {
+            userName,
+            password,
+            type: "admin"
+        })
+        userId = signupResponse.data.userId
+        const response = await axios.post(BACKEND_URL + "/api/v1/signin", {
+            userName,
+            password
+        })
+        token = response.body.token;
+        const avatarResponse = await axios.post(BACKEND_URL + "/api/v1/admin/avatar", {
+            "image": "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+            "name": "Avatar 1"
+        })
+        avatarId=avatarResponse.data.avatarId
+    })
 
 
-
-
+})
 
 
