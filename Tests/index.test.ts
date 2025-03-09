b03245199a62a85a892ef2482a7b6fd072e75d93
Prathamesh Dalavi <prathameshdalavi04@gmail.com>
2025-03-09 12:49:38 +0530

    updated test

--- Tests/index.test.ts
+++ Tests/index.test.ts
@@ -136,6 +136,93 @@ describe("User avatar Information", () => {
         expect(currentAvatar).toBeDefined()
     })
 })
+describe("Space Information", () => {
+    let mapId: any;
+    let element1Id: any;
+    let element2Id: any;
+    let token: any ;
+    let adminId: any;
+    let adminToken: any;
+    let userId: string;
+    let userToken: any;
+    beforeAll(async () => {
+        const userName = `prathamesh-${Math.random()}`;
+        const password= "123456";
+        const signupResponse = await axios.post(BACKEND_URL + "/api/v1/signup", {
+            userName,
+            password,
+            type: "admin"
+        })
 
+        adminId = signupResponse.data.userId
+        const response = await axios.post(BACKEND_URL + "/api/v1/signin", {
+            userName,
+            password
+        })
+        adminToken   = response.data.token; 
+        
 
+        const userSignupResponse = await axios.post(BACKEND_URL + "/api/v1/signup", {
+            userName: `prathamesh-${Math.random()}`,
+            password,
+            type: "user"
+        })
 
+        userId = userSignupResponse.data.userId
+        const userSigninResponse = await axios.post(BACKEND_URL + "/api/v1/signin", {
+            userName: `prathamesh-${Math.random()}`,
+            password
+        })
+
+        userToken   = userSigninResponse.data.token;
+        
+        
+        
+        const element1= await axios.post(BACKEND_URL + "/api/v1/admin/element", {
+            "image": "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+            "width": 1,
+            "height": 1,
+            "static": true},{
+            headers: {
+                Authorization: `Bearer ${adminToken}` 
+            }
+        })
+        
+     
+        const element2= await axios.post(BACKEND_URL + "/api/v1/admin/element", {
+            "image": "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+            "width": 1,
+            "height": 1,
+            "static": true},{
+            headers: {
+                Authorization: `Bearer ${adminToken}`
+            }
+        })
+        element1Id=element1.data.elementId
+        element2Id=element2.data.elementId
+        const map= await axios.post(BACKEND_URL + "/api/v1/admin/map", {
+            "thumbnail": "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",  
+            "dimensions": "100x200",
+            "defaultElements":[{
+                elementId:element1Id,
+                x:20,
+                y:20
+            },
+            {
+                elementId:element1Id,
+                x:18,
+                y:20
+            },
+        {
+            elementId:element2Id, 
+            x:19,
+            y:20
+        }]},{
+            headers: {
+                Authorization: `Bearer ${adminToken}`
+            }
+        })
+        mapId=map.data.mapId
+    }
+    )
+})
